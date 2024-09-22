3520c54dc1d5dbd5241648e9b421edcf69da26e3
k <kyle.offenbacher@gmail.com>
2024-09-22 12:42:21 -0400

    Initialize inventory with product objects

---
+++ order_management_system.js
@@ -0,0 +1,10 @@
+//coding challenge 5
+
+//task 1
+
+const inventory = [
+  { name: 'Espresso', price: 3, quantity: 10 },
+  { name: 'Latte', price: 4, quantity: 5 },
+  { name: 'Cappuccino', price: 4, quantity: 6 },
+  { name: 'Mocha', price: 5, quantity: 4 }
+];
